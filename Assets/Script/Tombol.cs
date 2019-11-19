d2dea5458b6a2614b6b3323de50b0523ba5cf4e8
Obi Yasir Lubis <49540401+ObiYasir@users.noreply.github.com>
2019-11-19 22:20:03 +0700

    Add files via upload

---
+++ Assets/Script/Tombol.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Nama  : Obi Yasir Lubis
+//NPM   : 1614370311
+//Kelas : RPL-MALAM A
+public class Tombol : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+		
+	}
+	public void scene(string scene){
+		Application.LoadLevel (scene);
+	}
+	public void scale(float scale){
+		transform.localScale = new Vector2 (1/scale,1*scale);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
